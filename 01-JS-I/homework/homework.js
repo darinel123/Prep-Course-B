67b552941ddc90ee413554af6f41a207eca4f205
darinel123 <internetdiego80@gmail.com>
2022-09-14 20:11:12 -0500

    14 de sep

--- 01-JS-I/homework/homework.js
+++ 01-JS-I/homework/homework.js
@@ -1,22 +1,23 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Hola";
+var nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 16;
+var nuevoNum = 15;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+var nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - 5 === 5;
+var nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * 4 === 40 ;
+var nuevaMultiplicacion = 10 * 4 === 40 ;
+
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 1;
+var nuevoModulo = 21 % 5 === 1;
 
 // No modificar nada debajo de esta línea
 // --------------------------------
